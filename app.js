658ac93f40fa12dc4ada95d95be3f381c74afdc3
GardyVoir <clement.wilfart@gmail.com>
2021-03-02 16:48:47 +0100

    Désactivé les hashs Connecté a la base MongoDb au login

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ const hash = require("pbkdf2-password")();
 const path = require("path");
 var session = require("express-session");
 
+const { MongoClient } = require("mongodb");
+
+const url = 'mongodb+srv://User:cvGh1hh1QjKcsnfw@securitocluster.sbsua.mongodb.net/CoffreFort?retryWrites=true&w=majority'
+const client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true });
+
 // config
 
 app.set("view engine", "ejs");
@@ -35,35 +40,43 @@ app.use(function (req, res, next) {
 
 // dummy database
 
-var users = {
-  tj: { name: "tj" },
-};
+// var users = {
+//   tj: { name: "tj" },
+// };
 
 // when you create a user, generate a salt
 // and hash the password ('foobar' is the pass here)
 
-hash({ password: "foobar" }, function (err, pass, salt, hash) {
-  if (err) throw err;
-  // store the salt & hash in the "db"
-  users.tj.salt = salt;
-  users.tj.hash = hash;
-});
+// hash({ password: "foobar" }, function (err, pass, salt, hash) {
+//   if (err) throw err;
+//   // store the salt & hash in the "db"
+//   users.tj.salt = salt;
+//   users.tj.hash = hash;
+// });
 
 // Authenticate using our plain-object database of doom!
 
-function authenticate(name, pass, fn) {
+async function authenticate(name, pass, fn) {
   if (!module.parent) console.log("authenticating %s:%s", name, pass);
-  var user = users[name];
+  // var user = users[name];
   // query the db for the given username
+
+  await client.connect();
+  const database = client.db('CoffreFort');
+  const Users = database.collection('Users');
+  var user = await Users.findOne({ utilisateur: name, mdp: pass })
   if (!user) return fn(new Error("cannot find user"));
   // apply the same algorithm to the POSTed password, applying
   // the hash against the pass / salt, if there is a match we
   // found the user
-  hash({ password: pass, salt: user.salt }, function (err, pass, salt, hash) {
-    if (err) return fn(err);
-    if (hash === user.hash) return fn(null, user);
-    fn(new Error("invalid password"));
-  });
+
+  // hash({ password: pass, salt: user.salt }, function (err, pass, salt, hash) {
+  //   if (err) return fn(err);
+  //   if (hash === user.hash) return fn(null, user);
+  //   fn(new Error("invalid password"));
+  // });
+  await client.close();
+  return fn(null, user);
 }
 
 function restrict(req, res, next) {
@@ -107,16 +120,15 @@ app.post("/login", function (req, res) {
         req.session.user = user;
         req.session.success =
           "Authenticated as " +
-          user.name +
+          user.utilisateur +
           ' click to <a href="/logout">logout</a>. ' +
           ' You may now access <a href="/restricted">/restricted</a>.';
-        res.redirect("back");
+        res.redirect("back"); //Rediriger directement sur la page d'upload
       });
     } else {
       req.session.error =
         "Authentication failed, please check your " +
-        " username and password." +
-        ' (use "tj" and "foobar")';
+        " username and password.";
       res.redirect("/login");
     }
   });
